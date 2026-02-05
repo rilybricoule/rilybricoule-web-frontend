4260b8312f9ed4e61ec575ee87a40d66ad3ce9b2
Chaimae <chaimaekaz05@gmail.com>
2026-02-05 20:35:03 +0100

    feat: add landing page

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Footer from "../components/Footer";
+import "../assets/styles/landing.css";
+
+function LandingPage() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  );
+}
+
+export default LandingPage;
